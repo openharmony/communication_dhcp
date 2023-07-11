167b998489f4eb680201b55b00c18e9d82093a29
sunjingqiang <17319862417@163.com>
2023-07-11 11:37:30 +0800

    DHCP新增用例
    Signed-off-by: sunjingqiang <17319862417@163.com>

--- test/services/dhcp_client/dhcp_ipv4_test.cpp
+++ test/services/dhcp_client/dhcp_ipv4_test.cpp
@@ -83,59 +83,109 @@ HWTEST_F(DhcpIpv4Test, TEST_FAILED, TestSize.Level1)
     EXPECT_EQ(DHCP_OPT_FAILED, GetPacketHeaderInfo(NULL, 0));
     EXPECT_EQ(DHCP_OPT_FAILED, GetPacketCommonInfo(NULL));
 }
-
+/**
+ * @tc.name: DhcpRenew_FAILED
+ * @tc.desc: DhcpRenew()
+ * @tc.type: FUNC
+ * @tc.require: issue
+*/
 HWTEST_F(DhcpIpv4Test, DhcpRenew_FAILED, TestSize.Level1)
 {
     EXPECT_EQ(SOCKET_OPT_FAILED, DhcpRenew(1, 0, 0));
 }
-
+/**
+ * @tc.name: DhcpRenew_SUCCESS
+ * @tc.desc: DhcpRenew()
+ * @tc.type: FUNC
+ * @tc.require: issue
+*/
 HWTEST_F(DhcpIpv4Test, DhcpRenew_SUCCESS, TestSize.Level1)
 {
     EXPECT_EQ(SOCKET_OPT_FAILED, DhcpRenew(1, 0, 1));
 }
-
+/**
+ * @tc.name: DhcpRequest_SUCCESS
+ * @tc.desc: DhcpRequest()
+ * @tc.type: FUNC
+ * @tc.require: issue
+*/
 HWTEST_F(DhcpIpv4Test, DhcpRequest_SUCCESS, TestSize.Level1)
 {
     EXPECT_EQ(SOCKET_OPT_FAILED, DhcpRequest(1, 0, 1));
 }
-
+/**
+ * @tc.name: DhcpDiscover_SUCCESS
+ * @tc.desc: DhcpDiscover()
+ * @tc.type: FUNC
+ * @tc.require: issue
+*/
 HWTEST_F(DhcpIpv4Test, DhcpDiscover_SUCCESS, TestSize.Level1)
 {
     EXPECT_EQ(SOCKET_OPT_FAILED, DhcpDiscover(1, 0));
 }
-
+/**
+ * @tc.name: DhcpDiscover_SUCCESS2
+ * @tc.desc: DhcpDiscover()
+ * @tc.type: FUNC
+ * @tc.require: issue
+*/
 HWTEST_F(DhcpIpv4Test, DhcpDiscover_SUCCESS2, TestSize.Level1)
 {
     EXPECT_EQ(SOCKET_OPT_FAILED, DhcpDiscover(0, 1));
 }
-
+/**
+ * @tc.name: PublishDhcpResultEvent_Fail1
+ * @tc.desc: PublishDhcpResultEvent()
+ * @tc.type: FUNC
+ * @tc.require: issue
+*/
 HWTEST_F(DhcpIpv4Test, PublishDhcpResultEvent_Fail1, TestSize.Level1)
 {
     DhcpResult result;
     EXPECT_EQ(DHCP_OPT_FAILED, PublishDhcpResultEvent(nullptr, PUBLISH_CODE_SUCCESS, &result));
 }
-
+/**
+ * @tc.name: PublishDhcpResultEvent_Fail2
+ * @tc.desc: PublishDhcpResultEvent()
+ * @tc.type: FUNC
+ * @tc.require: issue
+*/
 HWTEST_F(DhcpIpv4Test, PublishDhcpResultEvent_Fail2, TestSize.Level1)
 {
     DhcpResult result;
     char ifname[] = "testcode//";
     EXPECT_EQ(DHCP_OPT_FAILED, PublishDhcpResultEvent(ifname, DHCP_HWADDR_LENGTH, &result));
 }
-
+/**
+ * @tc.name: PublishDhcpResultEvent_Fail3
+ * @tc.desc: PublishDhcpResultEvent()
+ * @tc.type: FUNC
+ * @tc.require: issue
+*/
 HWTEST_F(DhcpIpv4Test, PublishDhcpResultEvent_Fail3, TestSize.Level1)
 {
     DhcpResult *result = NULL;
     char ifname[] = "testcode//";
     EXPECT_EQ(DHCP_OPT_FAILED, PublishDhcpResultEvent(ifname, PUBLISH_CODE_SUCCESS, result));
 }
-
+/**
+ * @tc.name: PublishDhcpResultEvent_Fail4
+ * @tc.desc: PublishDhcpResultEvent()
+ * @tc.type: FUNC
+ * @tc.require: issue
+*/
 HWTEST_F(DhcpIpv4Test, PublishDhcpResultEvent_Fail4, TestSize.Level1)
 {
     DhcpResult result;
     char ifname[] = "testcode//";
     EXPECT_EQ(DHCP_OPT_SUCCESS, PublishDhcpResultEvent(ifname, PUBLISH_CODE_SUCCESS, &result));
 }
-
+/**
+ * @tc.name: PublishDhcpResultEvent_Fail5
+ * @tc.desc: PublishDhcpResultEvent()
+ * @tc.type: FUNC
+ * @tc.require: issue
+*/
 HWTEST_F(DhcpIpv4Test, PublishDhcpResultEvent_Fail5, TestSize.Level1)
 {
     DhcpResult result;
