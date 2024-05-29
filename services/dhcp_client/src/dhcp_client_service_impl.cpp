43ff7be24bbedee2de4bcf753a334c201136fd81
CoderAdo <duliqun1@h-partners.com>
2024-05-29 10:08:53 +0000

    update services/dhcp_client/src/dhcp_client_service_impl.cpp.
    Signed-off-by: CoderAdo <duliqun1@h-partners.com>

--- services/dhcp_client/src/dhcp_client_service_impl.cpp
+++ services/dhcp_client/src/dhcp_client_service_impl.cpp
@@ -456,6 +456,36 @@ int DhcpClientServiceImpl::DhcpIpv4ResultFail(struct DhcpIpResult &ipResult)
     return OHOS::DHCP::DHCP_OPT_SUCCESS;
 }
 
+int DhcpClientServiceImpl::DhcpIpv4ResultTimeOut(const std::string &ifname)
+{
+    DHCP_LOGI("DhcpIpv4ResultTimeOut ifname:%{public}s", ifname.c_str());
+    ActionMode action = ACTION_INVALID;
+    {
+        std::lock_guard<std::mutex> autoLock(m_clientServiceMutex);
+        auto iterlient = m_mapClientService.find(ifname);
+        if (iterlient != m_mapClientService.end() && ((iterlient->second).pStaStateMachine != nullptr)) {
+            action = (iterlient->second).pStaStateMachine->GetAction();
+        }
+    }
+    std::lock_guard<std::mutex> autoLock(m_clientCallBackMutex);
+    auto iter = m_mapClientCallBack.find(ifname);
+    if (iter == m_mapClientCallBack.end()) {
+        DHCP_LOGE("DhcpIpv4ResultTimeOut m_mapClientCallBack not find callback!");
+        return OHOS::DHCP::DHCP_OPT_FAILED;
+    }
+    if ((iter->second) == nullptr) {
+        DHCP_LOGE("DhcpIpv4ResultTimeOut mclientCallback == nullptr!");
+        return OHOS::DHCP::DHCP_OPT_FAILED;
+    }
+    if ((action == ACTION_RENEW_T1) || (action == ACTION_RENEW_T2) || (action == ACTION_RENEW_T2)) {
+        (iter->second)->OnIpFailChanged(DHCP_OPT_RENEW_TIMEOUT, ifname.c_str(), "get dhcp renew result timeout!");
+    } else {
+        (iter->second)->OnIpFailChanged(DHCP_OPT_TIMEOUT, ifname.c_str(), "get dhcp result timeout!");
+    }
+    DHCP_LOGI("DhcpIpv4ResultTimeOut OnIpFailChanged Timeout!, action:%{public}d", action);
+    return OHOS::DHCP::DHCP_OPT_SUCCESS;
+}
+
 int DhcpClientServiceImpl::DhcpIpv4ResultExpired(const std::string &ifname)
 {
     DHCP_LOGI("DhcpIpv4ResultExpired ifname:%{public}s", ifname.c_str());
