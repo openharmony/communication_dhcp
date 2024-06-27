7ed0fbaff6e8bc21824ad5c1c8a9daab44034509
CodeAdo <baliguan163@163.com>
2024-06-27 11:49:34 +0800

    dhcp fix fuzz and tdd Signed-off-by: CoderAdo <duliqun1@h-partners.com>

--- test/fuzztest/clientstub_fuzzer/clientstub_fuzzer.cpp
+++ test/fuzztest/clientstub_fuzzer/clientstub_fuzzer.cpp
@@ -75,6 +75,19 @@ void OnStopDhcpClientTest(const std::string& ifname, size_t size, bool ipv6)
     pDhcpClientStub->OnRemoteRequest(code, datas, reply, option);
 }
 
+void OnSetConfigurationTest(const std::string& ifname)
+{
+    uint32_t code = static_cast<uint32_t>(DhcpClientInterfaceCode::DHCP_CLIENT_SVR_CMD_SET_CONFIG);
+    MessageParcel datas;
+    datas.WriteInterfaceToken(FORMMGR_INTERFACE_TOKEN);
+    datas.WriteInt32(0);
+    datas.WriteString(ifname);
+    datas.WriteString("bssid");
+    MessageParcel reply;
+    MessageOption option;
+    pDhcpClientStub->OnRemoteRequest(code, datas, reply, option);
+}
+
 /* Fuzzer entry point */
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
@@ -84,11 +97,12 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
     std::string ifname = "wlan0";
     OnRegisterCallBackTest(ifname, size);
     sleep(DHCP_SLEEP_1);
-    OnStartDhcpClientTest(ifname, size, false); // max 15s timeout
+    OnSetConfigurationTest(ifname);
+    sleep(DHCP_SLEEP_1);
+    OnStartDhcpClientTest(ifname, size, false); // max 18s timeout
     sleep(DHCP_SLEEP_2);
     OnStopDhcpClientTest(ifname, size, false);
     sleep(DHCP_SLEEP_1);
-    OnStopDhcpClientTest(ifname, size, true);
     return 0;
 }
 }  // namespace DHCP
