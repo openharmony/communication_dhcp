2c83af7deabb174dfc604acf3cc67c807de56a7b
HW-cjd <chengjiadong@huawei.com>
2024-09-25 08:17:15 +0000

    update services/dhcp_client/include/dhcp_ipv6_client.h.
    Signed-off-by: HW-cjd <chengjiadong@huawei.com>

--- services/dhcp_client/include/dhcp_ipv6_client.h
+++ services/dhcp_client/include/dhcp_ipv6_client.h
@@ -80,6 +80,7 @@ private:
     void getIpv6RouteAddr();
     void fillRouteData(char* buff, int &len);
     bool IsEui64ModeIpv6Address(char *ipv6addr, int len);
+
     std::function<void(const std::string ifname, DhcpIpv6Info &info)> onIpv6AddressChanged;
     std::string interfaceName;
     struct DhcpIpv6Info dhcpIpv6Info;
