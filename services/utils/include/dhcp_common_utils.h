87e80eef327632dabdc6d1c632dc441c82079e05
CodeAdo <baliguan163@163.com>
2024-10-09 14:51:35 +0800

    fix crash
    Signed-off-by: CodeAdo <baliguan163@163.com>

--- services/utils/include/dhcp_common_utils.h
+++ services/utils/include/dhcp_common_utils.h
@@ -33,6 +33,7 @@ char *UintIp4ToStr(uint32_t uIp, bool bHost);
 std::string IntIpv4ToAnonymizeStr(uint32_t ip);
 std::string MacArray2Str(uint8_t *macArray, int32_t len);
 int CheckDataLegal(std::string &data);
+int64_t GetElapsedSecondsSinceBoot();
 }
 }
 #endif
