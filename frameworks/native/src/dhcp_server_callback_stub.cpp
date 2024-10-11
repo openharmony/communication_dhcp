2205d26b745d83ba9ee28edcd7019055fedf18ca
sunflower-farmer <1628312209@qq.com>
2024-10-11 15:58:49 +0800

    Signed-off-by:kangwei<1628312209@qq.com>

--- frameworks/native/src/dhcp_server_callback_stub.cpp
+++ frameworks/native/src/dhcp_server_callback_stub.cpp
@@ -150,17 +150,17 @@ int DhcpServreCallBackStub::RemoteOnServerSuccess(uint32_t code, MessageParcel &
         std::string ipAddress = data.ReadString();
         DhcpStationInfo stationInfo;
         memset_s(&stationInfo, sizeof(DhcpStationInfo), 0, sizeof(DhcpStationInfo));
-        if (memcpy_s(stationInfo.ipAddr, sizeof(stationInfo.ipAddr), 
+        if (memcpy_s(stationInfo.ipAddr, sizeof(stationInfo.ipAddr),
                      ipAddress.c_str(), sizeof(stationInfo.ipAddr)) != EOK) {
             DHCP_LOGE("ipAddr memcpy_s error!");
             return DHCP_E_FAILED;
         }
-        if (memcpy_s(stationInfo.macAddr, sizeof(stationInfo.macAddr), 
+        if (memcpy_s(stationInfo.macAddr, sizeof(stationInfo.macAddr),
                      macAddress.c_str(), sizeof(stationInfo.macAddr)) != EOK) {
             DHCP_LOGE("macAddr memcpy_s error!");
             return DHCP_E_FAILED;
         }
-        if (memcpy_s(stationInfo.deviceName, sizeof(stationInfo.deviceName), 
+        if (memcpy_s(stationInfo.deviceName, sizeof(stationInfo.deviceName),
                      deviceName.c_str(), sizeof(stationInfo.deviceName)) != EOK) {
             DHCP_LOGE("deviceName memcpy_s error!");
             return DHCP_E_FAILED;
