418bb2596ea90198bef3431ff2b9262dd6d09e0d
HW-cjd <chengjiadong@huawei.com>
2024-10-26 03:39:01 +0000

    update test/unittest/services/dhcp_server/unittest/dhcp_server_service_impl_test.cpp.
    Signed-off-by: HW-cjd <chengjiadong@huawei.com>

--- test/unittest/services/dhcp_server/unittest/dhcp_server_service_impl_test.cpp
+++ test/unittest/services/dhcp_server/unittest/dhcp_server_service_impl_test.cpp
@@ -233,5 +233,35 @@ HWTEST_F(DhcpServerServiceTest, StartServiceAbilityTest, TestSize.Level1)
     pServerServiceImpl->StartServiceAbility(1);
     EXPECT_EQ(DHCP_OPT_SUCCESS, ZERO);
 }
+
+HWTEST_F(DhcpServerServiceTest, m_mapInfDhcpRange_TEST, TestSize.Level1)
+{
+    std::string ipRange = "192.168.1.100";
+    DhcpRange range;
+    range.strTagName = "test_ifname";
+    range.strStartip = "192.168.1.1";
+    range.strEndip = "192.168.1.100";
+    range.strSubnet = "255.255.255.0";
+    range.iptype = 0;
+    DhcpRange putRange;
+    putRange.iptype = 0;
+    pServerServiceImpl->m_mapInfDhcpRange["test_ifname"];
+    EXPECT_EQ(pServerServiceImpl->GetUsingIpRange("test_ifname", ipRange), DHCP_OPT_FAILED);
+    EXPECT_EQ(pServerServiceImpl->SetDhcpRange("test_ifname", range), DHCP_E_FAILED);
+    EXPECT_EQ(pServerServiceImpl->StopDhcpServer("test_ifname"), DHCP_E_SUCCESS);
+    EXPECT_EQ(pServerServiceImpl->CheckAndUpdateConf("test_ifname"), DHCP_E_SUCCESS);
+}
+
+HWTEST_F(DhcpServerServiceTest, PutDhcpRangeTest, TestSize.Level1)
+{
+    DhcpRange range;
+    range.strTagName = "test_tag";
+    range.strStartip = "192.168.1.1";
+    range.strEndip = "192.168.1.100";
+    range.strSubnet = "255.255.255.0";
+    range.iptype = 0;
+    pServerServiceImpl->m_mapTagDhcpRange["test_tag"];
+    EXPECT_EQ(pServerServiceImpl->PutDhcpRange("test_tag", range), DHCP_E_SUCCESS);
+    EXPECT_EQ(pServerServiceImpl->SetDhcpNameExt("test_tag", "test_tag"), DHCP_E_FAILED);
 }
 }
