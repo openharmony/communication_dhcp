cb8696004186618651d6219854bb7f1eca6348af
zhangqin88 <zhangqin88@huawei.com>
2024-12-10 10:26:33 +0800

    fix dhcp crash
    Signed-off-by: zhangqin88 <zhangqin88@huawei.com>

--- services/dhcp_client/include/dhcp_client_state_machine.h
+++ services/dhcp_client/include/dhcp_client_state_machine.h
@@ -63,7 +63,7 @@ private:
     void ParseDhcpAckPacket(const struct DhcpPacket *packet, time_t timestamp);
     void ParseDhcpNakPacket(const struct DhcpPacket *packet, time_t timestamp);
     void DhcpInit(void);
-    bool InitSocketFd(int &sockFd);
+    bool InitSocketFd(int &sockFdRaw, int &sockFdkernel);
     void SetSocketMode(uint32_t mode);
     void ParseNetworkServerIdInfo(const struct DhcpPacket *packet, struct DhcpIpResult *result);
     void ParseNetworkInfo(const struct DhcpPacket *packet, struct DhcpIpResult *result);
