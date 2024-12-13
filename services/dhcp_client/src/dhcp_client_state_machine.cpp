99f424d2eb664573079c4de4fa7243430b6b8590
shenqihang <shenqihang@huawei.com>
2024-12-13 01:48:45 +0000

    修正socket初始化逻辑
    Signed-off-by: shenqihang <shenqihang@huawei.com>

--- services/dhcp_client/src/dhcp_client_state_machine.cpp
+++ services/dhcp_client/src/dhcp_client_state_machine.cpp
@@ -392,7 +392,6 @@ bool DhcpClientStateMachine::InitSocketFd(int &sockFdRaw, int &sockFdkernel)
     if (BindRawSocket(sockFdRaw, m_cltCnf.ifaceIndex, NULL) != SOCKET_OPT_SUCCESS) {
         DHCP_LOGE("InitSocketFd BindRawSocket failed, fd:%{public}d,index:%{public}d failed!",
             sockFdRaw, m_cltCnf.ifaceIndex);
-        close(sockFdRaw);
         return false;
     }
 
@@ -406,7 +405,6 @@ bool DhcpClientStateMachine::InitSocketFd(int &sockFdRaw, int &sockFdkernel)
         DHCP_LOGE("InitSocketFd BindKernelSocket failed, fd:%{public}d,ifname:%{public}s failed!",
             sockFdkernel, m_cltCnf.ifaceName);
         close(sockFdRaw);
-        close(sockFdkernel);
         return false;
     }
     
