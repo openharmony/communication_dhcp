4826e7ebf791a579c395474c7144760f9d8f46cf
shenqihang <shenqihang@huawei.com>
2025-02-05 18:08:56 +0800

    fix OnIpSuccessChanged crash
    Signed-off-by: shenqihang <shenqihang@huawei.com>

--- services/dhcp_client/src/dhcp_client_callback_proxy.cpp
+++ services/dhcp_client/src/dhcp_client_callback_proxy.cpp
@@ -26,19 +26,8 @@ DhcpClientCallbackProxy::DhcpClientCallbackProxy(const sptr<IRemoteObject> &impl
 DhcpClientCallbackProxy::~DhcpClientCallbackProxy()
 {}
 
-void DhcpClientCallbackProxy::OnIpSuccessChanged(int status, const std::string& ifname, DhcpResult& result)
+void DhcpClientCallbackProxy::WriteDhcpResult(const DhcpResult& result, MessageParcel &data)
 {
-    DHCP_LOGI("WifiDeviceCallBackProxy::OnIpSuccessChanged");
-    MessageOption option;
-    MessageParcel data;
-    MessageParcel reply;
-    if (!data.WriteInterfaceToken(GetDescriptor())) {
-        DHCP_LOGE("Write interface token error: %{public}s", __func__);
-        return;
-    }
-    data.WriteInt32(0);
-    data.WriteInt32(status);
-    data.WriteString(ifname);
     data.WriteInt32(result.iptype);
     data.WriteBool(result.isOptSuc);
     data.WriteInt32(result.uLeaseTime);
@@ -57,12 +46,32 @@ void DhcpClientCallbackProxy::OnIpSuccessChanged(int status, const std::string& 
     data.WriteString(result.strLocalAddr1);
     data.WriteString(result.strLocalAddr2);
     data.WriteInt32(result.vectorDnsAddr.size());
-    for (auto dns : result.vectorDnsAddr) {
+    for (auto &dns : result.vectorDnsAddr) {
         data.WriteString(dns);
     }
-    
-    DHCP_LOGI("start send client request");
-    int error = Remote()->SendRequest(static_cast<uint32_t>(DhcpClientInterfaceCode::DHCP_CLIENT_CBK_CMD_IP_SUCCESS_CHANGE), data, reply, option);
+}
+
+void DhcpClientCallbackProxy::OnIpSuccessChanged(int status, const std::string& ifname, DhcpResult& result)
+{
+    DHCP_LOGI("WifiDeviceCallBackProxy::OnIpSuccessChanged");
+    MessageOption option;
+    MessageParcel data;
+    MessageParcel reply;
+    if (!data.WriteInterfaceToken(GetDescriptor())) {
+        DHCP_LOGE("Write interface token error: %{public}s", __func__);
+        return;
+    }
+    sptr<IRemoteObject> remote = Remote();
+    if (remote == nullptr) {
+        DHCP_LOGE("Remote is null");
+        return;
+    }
+    data.WriteInt32(0);
+    data.WriteInt32(status);
+    data.WriteString(ifname);
+    WriteDhcpResult(result, data);
+    int error = remote->SendRequest(
+        static_cast<uint32_t>(DhcpClientInterfaceCode::DHCP_CLIENT_CBK_CMD_IP_SUCCESS_CHANGE), data, reply, option);
     if (error != ERR_NONE) {
         DHCP_LOGE("Set Attr(%{public}d) failed,error code is %{public}d",
             static_cast<int32_t>(DhcpClientInterfaceCode::DHCP_CLIENT_CBK_CMD_IP_SUCCESS_CHANGE), error);
@@ -73,7 +82,6 @@ void DhcpClientCallbackProxy::OnIpSuccessChanged(int status, const std::string& 
         DHCP_LOGE("notify wifi state change failed!");
     }
     DHCP_LOGI("send client request success");
-
     return;
 }
 
@@ -87,11 +95,17 @@ void DhcpClientCallbackProxy::OnIpFailChanged(int status, const std::string& ifn
         DHCP_LOGE("Write interface token error: %{public}s", __func__);
         return;
     }
+    sptr<IRemoteObject> remote = Remote();
+    if (remote == nullptr) {
+        DHCP_LOGE("Remote is null");
+        return;
+    }
     data.WriteInt32(0);
     data.WriteInt32(status);
     data.WriteString(ifname);
     data.WriteString(reason);
-    int error = Remote()->SendRequest(static_cast<uint32_t>(DhcpClientInterfaceCode::DHCP_CLIENT_CBK_CMD_IP_FAIL_CHANGE), data, reply, option);
+    int error = remote->SendRequest(
+        static_cast<uint32_t>(DhcpClientInterfaceCode::DHCP_CLIENT_CBK_CMD_IP_FAIL_CHANGE), data, reply, option);
     if (error != ERR_NONE) {
         DHCP_LOGE("Set Attr(%{public}d) failed,error code is %{public}d",
             static_cast<int32_t>(DhcpClientInterfaceCode::DHCP_CLIENT_CBK_CMD_IP_FAIL_CHANGE), error);
@@ -116,31 +130,16 @@ void DhcpClientCallbackProxy::OnDhcpOfferReport(int status, const std::string& i
         DHCP_LOGE("Write interface token error: %{public}s", __func__);
         return;
     }
+    sptr<IRemoteObject> remote = Remote();
+    if (remote == nullptr) {
+        DHCP_LOGE("Remote is null");
+        return;
+    }
     data.WriteInt32(0);
     data.WriteInt32(status);
     data.WriteString(ifname);
-    data.WriteInt32(result.iptype);
-    data.WriteBool(result.isOptSuc);
-    data.WriteInt32(result.uLeaseTime);
-    data.WriteInt32(result.uAddTime);
-    data.WriteInt32(result.uGetTime);
-    data.WriteString(result.strYourCli);
-    data.WriteString(result.strServer);
-    data.WriteString(result.strSubnet);
-    data.WriteString(result.strDns1);
-    data.WriteString(result.strDns2);
-    data.WriteString(result.strRouter1);
-    data.WriteString(result.strRouter2);
-    data.WriteString(result.strVendor);
-    data.WriteString(result.strLinkIpv6Addr);
-    data.WriteString(result.strRandIpv6Addr);
-    data.WriteString(result.strLocalAddr1);
-    data.WriteString(result.strLocalAddr2);
-    data.WriteInt32(result.vectorDnsAddr.size());
-    for (auto dns : result.vectorDnsAddr) {
-        data.WriteString(dns);
-    }
-    int error = Remote()->SendRequest(static_cast<uint32_t>(DhcpClientInterfaceCode::DHCP_CLIENT_CBK_CMD_DHCP_OFFER),
+    WriteDhcpResult(result, data);
+    int error = remote->SendRequest(static_cast<uint32_t>(DhcpClientInterfaceCode::DHCP_CLIENT_CBK_CMD_DHCP_OFFER),
         data, reply, option);
     if (error != ERR_NONE) {
         DHCP_LOGE("Set Attr(%{public}d) failed,error code is %{public}d",
