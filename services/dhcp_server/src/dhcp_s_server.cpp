1607b18f01a06bc04cc00742b354986d9a8544a6
tradeWindToNorth <wangxu262@h-partners.com>
2026-03-06 14:39:46 +0800

    1
    Signed-off-by: tradeWindToNorth <wangxu262@h-partners.com>

--- services/dhcp_server/src/dhcp_s_server.cpp
+++ services/dhcp_server/src/dhcp_s_server.cpp
@@ -1240,6 +1240,10 @@ int GetHostNameOption(PDhcpMsgInfo received, AddressBinding *bindin)
     }
     PDhcpOption optHostName = GetOption(&received->options, HOST_NAME_OPTION);
     if (optHostName) {
+        if (optHostName->length >= DEVICE_NAME_STRING_LENGTH) {
+              DHCP_LOGE("GetHostNameOption length too large");
+              return REPLY_NONE;
+        }
         if (memcpy_s(bindin->deviceName, DEVICE_NAME_STRING_LENGTH, (char*)optHostName->data,
             optHostName->length) != EOK) {
             DHCP_LOGE("GetHostNameOption pHost memcpy_s failed!");
