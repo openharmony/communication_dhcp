80f96d1dda24ef1eef07736da60fa0b33baa4e0c
tradeWindToNorth <wangxu262@h-partners.com>
2026-03-16 16:21:31 +0800

    update: 更新文件 dhcp_client_callback_stub.h
    Signed-off-by: tradeWindToNorth <wangxu262@h-partners.com>

--- frameworks/native/src/dhcp_client_callback_stub.h
+++ frameworks/native/src/dhcp_client_callback_stub.h
@@ -43,6 +43,7 @@ private:
     int RemoteOnIpFailChanged(uint32_t code, MessageParcel &data, MessageParcel &reply);
     int RemoteOnDhcpOfferReport(uint32_t code, MessageParcel &data, MessageParcel &reply);
 
+    sptr<IDhcpClientCallBack> callback_;
     std::mutex callbackMutex_;
     std::atomic<bool> mRemoteDied_;
 };
